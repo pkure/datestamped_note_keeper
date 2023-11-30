9c8f09c47e221dc6fce6e281331b7c6243f16d74
pkure <108906109+pkure@users.noreply.github.com>
2023-11-29 18:37:28 -0800

    Update script.js
    Added browser persistence

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  loadNotes(); // Load notes when the page loads
+});
+
 document
   .getElementById("new-note")
   .addEventListener("keypress", function (event) {
@@ -11,21 +15,41 @@ function addNote() {
   const noteElement = document.getElementById("new-note");
   const note = noteElement.value;
   noteElement.value = "";
+
   if (note.length < 2) {
     return alert("Please include more text to create a note.");
   }
 
   const list = document.getElementById("list");
   const li = document.createElement("li");
-
   const timestamp = new Date().toLocaleString();
+  const noteText = `${note} - ${timestamp}`;
 
-  const noteText = document.createTextNode(`${note}   -   ${timestamp}`);
-  const noteTimestamp = document.createElement("span");
-  noteTimestamp.className = "timestamp";
-  noteTimestamp.appendChild(noteText);
-
-  li.appendChild(noteTimestamp);
-
+  li.appendChild(document.createTextNode(noteText));
   list.appendChild(li);
+
+  saveNoteToLocalStorage(noteText); // Save the note to local storage
+}
+
+function saveNoteToLocalStorage(note) {
+  let notes = [];
+  if (localStorage.getItem("notes")) {
+    notes = JSON.parse(localStorage.getItem("notes"));
+  }
+  notes.push(note);
+  localStorage.setItem("notes", JSON.stringify(notes));
+}
+
+function loadNotes() {
+  const list = document.getElementById("list");
+  list.innerHTML = ""; // Clear existing notes
+
+  if (localStorage.getItem("notes")) {
+    const notes = JSON.parse(localStorage.getItem("notes"));
+    notes.forEach((note) => {
+      const li = document.createElement("li");
+      li.appendChild(document.createTextNode(note));
+      list.appendChild(li);
+    });
+  }
 }
